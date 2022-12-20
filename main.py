e83deafd2946810308c07763789b21967b4a9531
pylearncentral <120634396+pylearncentral@users.noreply.github.com>
2022-12-20 17:53:41 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,7 @@
+while True:
+    print("Welcome to School Management Software v01")
+    print("1. Display Student Details")
+    print("2. Update Student's Total Marks and Grade")
+    print("3. Enter Student Details")
+    print("e. Exit")
+    x = input("Please select: ")
