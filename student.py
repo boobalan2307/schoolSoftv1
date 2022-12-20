be69f59ecca7d800e295fd0e3bc81dabf985aa7c
pylearncentral <120634396+pylearncentral@users.noreply.github.com>
2022-12-20 17:55:22 +0530

    Create student.py

---
+++ student.py
@@ -0,0 +1,13 @@
+def calc_total_marks(s):
+    """A student dictionary is passed in as input.
+    Total marks is calculated from this dictionary and returned."""
+    
+def calc_grade(s):
+    """A student dictionary is passed in as input.
+    grade is calculated from this dictionary and returned."""
+    
+def load_student_info():
+    """Reads data from file and returns list of student dictionaries"""
+    
+def enter_student_details():
+    """Gets student's info from user, writes it to a file and return a student dictionary"""
